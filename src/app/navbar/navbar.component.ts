6ac1b22156287c939fd021ecc7f68e9135461c63
2AW3 User 2 <2AW3-2@yourdomain.com>
2025-01-20 10:26:54 +0100

    login hecho y navbar actualizado

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthServService } from '../services/auth-serv.service';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-navbar',
@@ -7,7 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit{
 
+  isAdmin: boolean = false;
+  is_logged: boolean = false;
+  userName: String = "";
+
+  constructor(private authService: AuthServService, private route: Router){ }
+
   ngOnInit(){
+
+    this.authService.admin$.subscribe(admin => {
+      this.isAdmin = admin;
+    });
+
+    this.authService.isLogged$.subscribe(logged => {
+      this.is_logged = logged;
+    });
+
+    this.authService.userName$.subscribe(name => {
+      this.userName = name;
+    });
+
     const links = document.querySelectorAll("a");
 
     links.forEach(link =>{
@@ -34,6 +56,28 @@ export class NavbarComponent implements OnInit{
 
   cerrarMenu(){
     this.abierto = false;
+  }
+
+  logout(): void {
+    this.authService.logout(); // Cerrar sesión
+    this.cerrarMenu();
+    this.route.navigate([""]);
+
+    const Toast = Swal.mixin({
+      toast: true,
+      position: "bottom",
+      showConfirmButton: false,
+      timer: 3000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.onmouseenter = Swal.stopTimer;
+        toast.onmouseleave = Swal.resumeTimer;
+      }
+    });
+    Toast.fire({
+      icon: "info",
+      title: "Has cerrado sesión"
+    });
   }
 
 }

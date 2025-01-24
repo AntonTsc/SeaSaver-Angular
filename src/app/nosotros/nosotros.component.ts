a8447659c959b449e4cba10be816d3da2e86dc40
IbaiAmo <i_amobal@fpzornotza.com>
2025-01-24 22:51:22 +0100

    Pagina sobre nosotros acabada

--- src/app/nosotros/nosotros.component.ts
+++ src/app/nosotros/nosotros.component.ts
@@ -7,20 +7,25 @@ import { TripuServService } from '../services/tripu-serv.service';
   styleUrls: ['./nosotros.component.scss']
 })
 export class NosotrosComponent implements OnInit{
-  trabajadores: any[] = [];
+  capitanes: any[] = [];
+  medicos: any[] = [];
+  tripulantes: any[] = []
 
   constructor(private servicio: TripuServService){}
 
   /**
    * Recogida de datos de la API, se hacen dos peticiones GET
    * y al final juntamos los objetos obtenidos con el operador ...
+   * esto es simplemente para que si no tuvieramos ningun
+   * tripulante pues que no se renderice esa parte.
    */
   ngOnInit(): void {
-    this.servicio.getCapitanes().subscribe(capitanes =>{
+    this.servicio.getCapitanes().subscribe(cap =>{
+      this.capitanes = cap;
 
-      this.servicio.getMedicos().subscribe(medicos =>{
-        this.trabajadores = [...capitanes, ...medicos];
-        console.log(this.trabajadores);        
+      this.servicio.getMedicos().subscribe(med =>{
+        this.medicos = med;
+        this.tripulantes = [...this.capitanes, ...this.medicos];    
       });
       
     });

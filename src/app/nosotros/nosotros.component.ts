5cb55dd2eaf6d461c6e7a77583fe7629d58709b8
IbaiAmo <i_amobal@fpzornotza.com>
2025-01-24 12:13:54 +0100

    nosotros

--- src/app/nosotros/nosotros.component.ts
+++ src/app/nosotros/nosotros.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
-import nosotros from 'src/assets/json/nosotros.json'
+import { Component, OnInit } from '@angular/core';
+import { TripuServService } from '../services/tripu-serv.service';
 
 @Component({
   selector: 'app-nosotros',
   templateUrl: './nosotros.component.html',
   styleUrls: ['./nosotros.component.scss']
 })
-export class NosotrosComponent {
-  nosotros:any=nosotros
+export class NosotrosComponent implements OnInit{
+  trabajadores: any[] = [];
+
+  constructor(private servicio: TripuServService){}
+
+  /**
+   * Recogida de datos de la API, se hacen dos peticiones GET
+   * y al final juntamos los objetos obtenidos con el operador ...
+   */
+  ngOnInit(): void {
+    this.servicio.getCapitanes().subscribe(capitanes =>{
+
+      this.servicio.getMedicos().subscribe(medicos =>{
+        this.trabajadores = [...capitanes, ...medicos];
+        console.log(this.trabajadores);        
+      });
+      
+    });
+    
+  }
 }

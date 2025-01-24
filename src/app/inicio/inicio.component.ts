da21ab69ed297e4516cec3a3fd8d0ba91b209ddc
IbaiAmo <i_amobal@fpzornotza.com>
2025-01-25 00:46:14 +0100

    api de rescates implementada

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
-import rescates from 'src/assets/json/rescates.json'
+import { Component, OnInit } from '@angular/core';
+import { RescatesService } from '../services/rescates.service';
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
   styleUrls: ['./inicio.component.scss'],
 })
-export class InicioComponent {
-  rescates: any[] = rescates;
+export class InicioComponent implements OnInit{
+  rescates: any[] = [];
+
+  constructor(private servicioResc: RescatesService){}
+
+  ngOnInit(): void {
+    this.servicioResc.getRescates().subscribe(resc=>{
+      this.rescates = [resc[0], resc[1], resc[2]];      
+    });
+  }
 }
---
+++ src/app/services/rescates.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RescatesService {
+
+  private url: string = "http://192.168.2.10/api/rescates";
+
+  constructor(private http: HttpClient) { }
+
+  getRescates(): Observable<any>{
+    return this.http.get(this.url);
+  }
+}
